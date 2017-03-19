d2ec138b53369d62498bc2ab03ab207d491c23cf
Hamza Ali <hamzali.tas.95@gmail.com>
2017-03-19 21:41:00 +0300

    Shape component added

--- index.android.js
+++ index.android.js
@@ -1,38 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  Alert,
+  Dimensions
+} from 'react-native'
+
+import axios from 'axios'
+
+import Svg,{
+    Circle,
+    Rect
+} from 'react-native-svg'
+
 
 export default class bitaksi_getir_mobile extends Component {
+  
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      shapes: []
+    };
+  }
+
+  componentWillMount () {
+    axios({
+      method: 'post',
+      url: 'https://getir-bitaksi-hackathon.herokuapp.com/getElements',
+      data: {
+        "email" : "hamzalitas95@gmail.com",
+        "name" : "Hamza Ali TAS",
+        "gsm" : "5545077657"
+      }
+    })
+    .then(res => {
+      
+      this.setState({shapes: res.data.elements});
+      
+    })
+    .catch(err => {
+      Alert.aler(err);
+    })
+  }
+
+  drawShapes = () => {
+    return this.state.shapes.map((shape, key) => {
+            if (shape.type === 'circle') {
+              return (
+                <Circle 
+                  key={key}
+                  cx={+shape.xPosition}
+                  cy={+shape.yPosition}
+                  r={+shape.r}
+                  fill={'#' + shape.color}/>
+              );
+            }
+
+            if (shape.type === 'rectangle') {
+              return (
+                <Rect
+                  key={key}
+                  x={+shape.xPosition}
+                  y={+shape.yPosition}
+                  width={+shape.width}
+                  height={+shape.height}
+                  fill={'#' + shape.color}/>
+              );
+            }
+          })
+  }
   render() {
+    let {height, width} = Dimensions.get('window');
     return (
-      <View style={styles.container}>
-        <Text>test 1</Text>
-      </View>
+        <Svg 
+          height={+height}
+          width={+width}>
+        {
+          this.drawShapes()
+        }
+        </Svg>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('bitaksi_getir_mobile', () => bitaksi_getir_mobile);
